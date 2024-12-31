0fb735b2664187f70a758dd5ddbb06c6251d8f99
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-12-30 21:56:27 -0300

    chore: add WordDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import HangmanFigure from "@/components/HangmanFigure";
+import WordDisplay from "@/components/WordDisplay";
 
 function App() {
   return (
@@ -6,6 +7,16 @@ function App() {
       <h1 className="text-4xl font-bold text-pink">Hangman</h1>
 
       <HangmanFigure remainingGuesses={6} />
+
+      <div className="text-center text-gray-100">
+        <p className="mb-2 text-xl sm:text-2xl">
+          Remaining guesses: <span className="font-bold">6</span>
+        </p>
+
+        <p className="text-lg">Tip: A dica é qualquer coisa</p>
+      </div>
+
+      <WordDisplay word="vercel" guessedLetters={new Set(["v", "e", "c"])} />
     </div>
   );
 }
